aceab28bb251b0f258638e171e5ba54002d29cc7
JayCode Developer <JayCode.contakt@gmail.com>
2023-07-27 01:10:30 +0200

    Style navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className="text-lg w-ful">
+      <ul className="flex gap-5 px-10 py-5">
         <li>
           <Link href="/">Blog</Link>
         </li>
