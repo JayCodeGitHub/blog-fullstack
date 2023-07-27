fd4b31f9d701ae72593e68494a925533cc7ab0a0
JayCode Developer <JayCode.contakt@gmail.com>
2023-07-27 16:40:50 +0200

    Add form to create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,9 +2,37 @@ import Navbar from "@/components/navbar";
 
 export default function Create() {
   return (
-    <main>
+    <>
       <Navbar />
-      <h1>Create</h1>
-    </main>
+      <main className="flex items-center justify-center pt-32">
+        <form className="w-full max-w-3xl mt-4 space-y-4">
+          <div>
+            <input
+              className="w-full"
+              placeholder="Title"
+              type="text"
+              name="title"
+              required
+            />
+          </div>
+          <div>
+            <textarea
+              className="w-full"
+              placeholder="Write something interesting..."
+              name="content"
+              required
+            />
+          </div>
+          <div className="text-right">
+            <button
+              type="submit"
+              className="px-3 py-1 font-medium text-white bg-sky-500"
+            >
+              Create post
+            </button>
+          </div>
+        </form>
+      </main>
+    </>
   );
 }
